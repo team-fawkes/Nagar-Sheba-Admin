9cd7032aa2d2243fbc5f35c4b6304fc84378befe
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-08-21 14:11:17 +0600

    Update PayController.php

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class PayController extends Controller
 {
     public function payment($invid){
-        $html = 'Payment in process. Invoice ID : '.$invid;
+        $invoice = Invoice::where('invid',$invid)->first();
+        $html = 'Payment Invoice not found';
+        if ($invoice){
+            $html = 'Payment in process. Invoice ID : '.$invid.
+                '<br> Invoice Amount is : '.$invoice->amount.
+                '<br> Invoice Status is : '.$invoice->status;
+        }
         return $html;
     }
 }
