c3e5e3edd47764a9a74dec2ca29a12965404bfd7
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-09-18 20:08:28 +0600

    Update ComplainOverview.php

--- app/Filament/Resources/ComplainResource/Widgets/ComplainOverview.php
+++ app/Filament/Resources/ComplainResource/Widgets/ComplainOverview.php
@@ -5,23 +5,31 @@ namespace App\Filament\Resources\ComplainResource\Widgets;
 use App\Models\Complain;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
+use Illuminate\Support\Facades\Auth;
 
 class ComplainOverview extends BaseWidget
 {
     protected function getCards(): array
     {
+        $service_category_id = Auth::user()->service_category_id;
+        $complain = Complain::all();
+        if ($service_category_id) {
+            $complain = Complain::where('service_category_id',Auth::user()->service_category_id)->get();
+        }
+
+
         return [
-            Card::make('Pending', Complain::where('status','pending')->count())
+            Card::make('Pending', $complain->where('status','pending')->count())
                 ->description('Total pending '.Complain::where('status','pending')->count())
                 ->color('danger'),
-            Card::make('Received', Complain::where('status','received')->count())
+            Card::make('Received', $complain->where('status','received')->count())
                 ->description('Total received '.Complain::where('status','received')->count())
                 ->color('warning'),
-            Card::make('Progress', Complain::where('status','progress')->count())
-                ->description('Total in progress '.Complain::where('status','progress')->count())
+            Card::make('Progress', $complain->where('status','progress')->count())
+                ->description('Total in progress '.$complain->where('status','progress')->count())
                 ->color('primary'),
-            Card::make('Solved', Complain::where('status','solved')->count())
-                ->description('Total '.Complain::where('status','solved')->count().' problems solved')
+            Card::make('Solved', $complain->where('status','solved')->count())
+                ->description('Total '.$complain->where('status','solved')->count().' problems solved')
                 ->color('success'),
 
         ];
